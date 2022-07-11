28e30dfc390596fad868ebe1c709ed037ad78703
AltaVista <altavista444@yandex.ru>
2022-07-12 00:03:54 +0300

    last fix

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -7,12 +7,11 @@ import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class MainApp {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
